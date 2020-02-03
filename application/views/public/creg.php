c99a11247d298c9995c0319c0af746fc5d097d8f
Oscar Josue Torres Molina <treotorres@crecer.com.sv>
2020-02-03 17:27:16 -0600

    Cambio html

--- application/views/public/creg.php
+++ application/views/public/creg.php
@@ -43,44 +43,5 @@
     <br>
     <hr>
     <br>  
-    <div class="col-md-12">
-        <div id="contenedor">
-            <h2>Tenemos</h2>
-            <div class="row">
-                <div class="col-md-3">
-                    <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
-                        <div class="card-header">Días</div>
-                        <div class="card-body">
-                            <h5 id="adias" class="card-title"></h5>                
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
-                        <div class="card-header">Horas</div>
-                        <div class="card-body">
-                            <h5 id="ahoras" class="card-title"></h5>                
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
-                        <div class="card-header">Min.</div>
-                        <div class="card-body">
-                            <h5 id="aminutos" class="card-title"></h5>                
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
-                        <div class="card-header">Seg.</div>
-                        <div class="card-body">
-                            <h5 id="asegundos" class="card-title"></h5>                
-                        </div>
-                    </div>
-                </div>                                
-            </div>
-            <h4><i>¡JUNTOS!</i></h4>
-        </div>
-    </div>
+    
 </div>
