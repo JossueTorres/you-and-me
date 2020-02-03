263781d51d35ec5e2a1e1e5788f82142866dbafb
Oscar Josue Torres Molina <treotorres@crecer.com.sv>
2020-02-03 17:42:35 -0600

    Cambios

--- application/views/public/cont.php
+++ application/views/public/cont.php
@@ -1,15 +1,15 @@
-<body onload="ConteoRegresivo()">
+<body onload="Conteo()">
 <div id="contenedor">
     <br>
-    <a href="<?= base_url()?>/you-and-me/Cont">¿Quieres saber cuánto tiempo tenemos Juntos?</a>
+    <a href="<?= base_url()?>/you-and-me/Creg">¿Quieres saber cuánto tiempo falta para el gran día?</a>
     <br>
-    <h2>Faltan</h2>
-    <div id="contador" class="row">
+    <h2>Tenemos</h2>
+    <div class="row">
         <div class="col-md-3">
             <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
                 <div class="card-header">Días</div>
                 <div class="card-body">
-                    <h5 id="dias" class="card-title"></h5>                
+                    <h5 id="adias" class="card-title"></h5>                
                 </div>
             </div>
         </div>
@@ -17,7 +17,7 @@
             <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
                 <div class="card-header">Horas</div>
                 <div class="card-body">
-                    <h5 id="horas" class="card-title"></h5>                
+                    <h5 id="ahoras" class="card-title"></h5>                
                 </div>
             </div>
         </div>
@@ -25,7 +25,7 @@
             <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
                 <div class="card-header">Min.</div>
                 <div class="card-body">
-                    <h5 id="minutos" class="card-title"></h5>                
+                    <h5 id="aminutos" class="card-title"></h5>                
                 </div>
             </div>
         </div>
@@ -33,14 +33,10 @@
             <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
                 <div class="card-header">Seg.</div>
                 <div class="card-body">
-                    <h5 id="segundos" class="card-title"></h5>                
+                    <h5 id="asegundos" class="card-title"></h5>                
                 </div>
             </div>
         </div>                                
     </div>
-    <h4><i>¡FALTA MUY POCO!</i></h4>
-    <div class="bottom">
-        <hr>
-        <hr>
-    </div>
+    <h4><i>¡JUNTOS!</i></h4>
 </div>
