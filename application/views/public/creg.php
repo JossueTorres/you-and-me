6018c44afe05143e21b9a15db3533a11dc681fa2
Oscar Josue Torres Molina <treotorres@crecer.com.sv>
2020-02-03 16:48:35 -0600

    Cambio diseño html

--- application/views/public/creg.php
+++ application/views/public/creg.php
@@ -7,14 +7,45 @@
 </head>
 <body onload="ConteoRegresivo()">
 <div id="contenedor">
-    <img src="talu.png">
-    <h2>Faltan:</h2>
-    <div id="contador"></div>
-    <h4><i>¡Para que empiece el Mundial de Fútbol Brasil 2014!</i></h4>
+    <h2>Faltan</h2>
+    <div id="contador" class="row">
+        <div class="col-md-3">
+            <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
+                <div class="card-header">Días</div>
+                <div class="card-body">
+                    <h5 id="dias" class="card-title"></h5>                
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
+                <div class="card-header">Horas</div>
+                <div class="card-body">
+                    <h5 id="horas" class="card-title"></h5>                
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
+                <div class="card-header">Minutos</div>
+                <div class="card-body">
+                    <h5 id="minutos" class="card-title"></h5>                
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
+                <div class="card-header">Segundos</div>
+                <div class="card-body">
+                    <h5 id="segundos" class="card-title"></h5>                
+                </div>
+            </div>
+        </div>                                
+    </div>
+    <h4><i>¡FALTA MUY POCO!</i></h4>
     <div class="bottom">
         <hr>
-        <p>Como hacer un conteo regresivo en HTML5, CSS3 y Javascript</p>
-        <p>http://www.genesisvargasj.com</p>
+        <hr>
     </div>
 </div>
 </body>
