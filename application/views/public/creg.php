6139050a69ff0350bb910e28cc8615f8abdcb588
Oscar Josue Torres Molina <treotorres@crecer.com.sv>
2020-02-03 17:05:12 -0600

    Maquetación y agregar conteo de días

--- application/views/public/creg.php
+++ application/views/public/creg.php
@@ -1,4 +1,4 @@
-<body onload="ConteoRegresivo()">
+<body onload="ConteoRegresivo(); Conteo();">
 <div id="contenedor">
     <h2>Faltan</h2>
     <div id="contador" class="row">
@@ -40,3 +40,43 @@
         <hr>
         <hr>
     </div>
+    <br>
+    <hr>
+    <br>
+    <h2>Tenemos</h2>
+    <div id="contador" class="row">
+        <div class="col-md-3">
+            <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
+                <div class="card-header">Días</div>
+                <div class="card-body">
+                    <h5 id="adias" class="card-title"></h5>                
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
+                <div class="card-header">Horas</div>
+                <div class="card-body">
+                    <h5 id="ahoras" class="card-title"></h5>                
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
+                <div class="card-header">Min.</div>
+                <div class="card-body">
+                    <h5 id="aminutos" class="card-title"></h5>                
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
+                <div class="card-header">Seg.</div>
+                <div class="card-body">
+                    <h5 id="asegundos" class="card-title"></h5>                
+                </div>
+            </div>
+        </div>                                
+    </div>
+    <h4><i>¡JUNTOS!</i></h4>
+</div>
--- assets/scripts/custom/creg.js
+++ assets/scripts/custom/creg.js
@@ -27,3 +27,30 @@ function ConteoRegresivo()
         }
     }
 }
+
+function Conteo()
+{
+    var fecha=new Date('2019','02','06', '12', '00')
+    var hoy=new Date()
+    var dias=0
+    var horas=0
+    var minutos=0
+    var segundos=0
+    var diferencia=(hoy.getTime()-fecha.getTime())/1000
+    dias=Math.floor(diferencia/86400)
+    diferencia=diferencia-(86400*dias)
+    horas=Math.floor(diferencia/3600)
+    diferencia=diferencia-(3600*horas)
+    minutos=Math.floor(diferencia/60)
+    diferencia=diferencia-(60*minutos)
+    segundos=Math.floor(diferencia)
+    // document.getElementById('contador').innerHTML = '' + dias + ' : ' + horas + ' : ' + minutos + ' : ' + segundos + ' Días  -  Horas  -  Minutos - Segundos';
+    document.getElementById('adias').innerHTML = '' + dias;
+    document.getElementById('ahoras').innerHTML = '' + horas;
+    document.getElementById('aminutos').innerHTML = '' + minutos;
+    document.getElementById('asegundos').innerHTML = '' + segundos;
+    if (dias>0 || horas>0 || minutos>0 || segundos>0)
+    {
+        setTimeout("Conteo()",1000);
+    }
+}
