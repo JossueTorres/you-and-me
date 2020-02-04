0ddf4712885c28efb2c15706cc28ec192595cca9
Oscar Josue Torres Molina <treotorres@crecer.com.sv>
2020-02-04 09:54:07 -0600

    Cambios html

--- application/views/public/cont.php
+++ application/views/public/cont.php
@@ -6,38 +6,39 @@
             <br>
             <a href="<?= base_url()?>Creg" class="btn btn-outline-danger col-md-12">¿Quieres saber cuánto tiempo falta para el gran día?</a>
             <br>
+            <br>
             <h2>Tenemos</h2>
         </div>
         <div class="row">
             <div class="col-md-3 col-xs-12">
                 <div class="card text-white bg-dark mb-3" style="min-width: 6rem;">
                     <div class="card-header">Días</div>
-                    <div class="card-body">
-                        <h5 id="adias" class="card-title"></h5>                
+                    <div class="card-body text-center">
+                        <h4 id="adias" class="card-title"></h4>                
                     </div>
                 </div>
             </div>
             <div class="col-md-3 col-xs-12">
                 <div class="card text-white bg-dark mb-3" style="min-width: 6rem;">
                     <div class="card-header">Hrs.</div>
-                    <div class="card-body">
-                        <h5 id="ahoras" class="card-title"></h5>                
+                    <div class="card-body text-center">
+                        <h4 id="ahoras" class="card-title"></h4>                
                     </div>
                 </div>
             </div>
             <div class="col-md-3 col-xs-12">
                 <div class="card text-white bg-dark mb-3" style="min-width: 6rem;">
                     <div class="card-header">Min.</div>
-                    <div class="card-body">
-                        <h5 id="aminutos" class="card-title"></h5>                
+                    <div class="card-body text-center">
+                        <h4 id="aminutos" class="card-title"></h4>                
                     </div>
                 </div>
             </div>
             <div class="col-md-3 col-xs-12">
                 <div class="card text-white bg-dark mb-3" style="min-width: 6rem;">
                     <div class="card-header">Seg.</div>
-                    <div class="card-body">
-                        <h5 id="asegundos" class="card-title"></h5>                
+                    <div class="card-body text-center">
+                        <h4 id="asegundos" class="card-title"></h4>                
                     </div>
                 </div>
             </div>                                
--- application/views/public/creg.php
+++ application/views/public/creg.php
@@ -1,42 +1,44 @@
 <body onload="ConteoRegresivo()">
+<div class="container"></div>
 <div class="container h-100">
     <div class="row h-100 justify-content-center align-items-center">
         <div class="col-md-12 text-center" style="margin-bottom:1%;margin-top:1%;">
             <br>
             <a href="<?= base_url()?>Cont" class="btn btn-outline-primary col-md-12">¿Quieres saber cuánto tiempo tenemos Juntos?</a>
             <br>
+            <br>            
             <h2>Faltan</h2>
         </div>        
         <div class="row">
             <div class="col-md-3 col-xs-12">
                 <div class="card text-white bg-dark mb-3" style="min-width: 6rem;">
                     <div class="card-header">Días</div>
-                    <div class="card-body">
-                        <h5 id="dias" class="card-title"></h5>                
+                    <div class="card-body text-center">
+                        <h4 id="dias" class="card-title"></h4>                
                     </div>
                 </div>
             </div>
             <div class="col-md-3 col-xs-12">
                 <div class="card text-white bg-dark mb-3" style="min-width: 6rem;">
                     <div class="card-header">Hrs.</div>
-                    <div class="card-body">
-                        <h5 id="horas" class="card-title"></h5>                
+                    <div class="card-body text-center">
+                        <h4 id="horas" class="card-title"></h4>                
                     </div>
                 </div>
             </div>
             <div class="col-md-3 col-xs-12">
                 <div class="card text-white bg-dark mb-3" style="min-width: 6rem;">
                     <div class="card-header">Min.</div>
-                    <div class="card-body">
-                        <h5 id="minutos" class="card-title"></h5>                
+                    <div class="card-body text-center">
+                        <h4 id="minutos" class="card-title"></h4>                
                     </div>
                 </div>
             </div>
             <div class="col-md-3 col-xs-12">
                 <div class="card text-white bg-dark mb-3" style="min-width: 6rem;">
                     <div class="card-header">Seg.</div>
-                    <div class="card-body">
-                        <h5 id="segundos" class="card-title"></h5>                
+                    <div class="card-body text-center">
+                        <h4 id="segundos" class="card-title"></h4>                
                     </div>
                 </div>
             </div>                                
