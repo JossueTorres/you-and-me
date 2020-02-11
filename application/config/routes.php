e6086a99a826cd5749823cfb6dbaf2e21cb186f2
Oscar Josue Torres Molina <treotorres@crecer.com.sv>
2020-02-11 08:20:44 -0600

    Cambiar Rutas Login

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'creg';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route['Login'] = 'Security/Login';
+$route['Login/Ingresar'] = 'Security/Auth/Login';
--- application/controllers/Security/Auth.php
+++ application/controllers/Security/Auth.php
@@ -3,7 +3,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
-
 	//Constructor del controlador: aca se agregan referencias a modelos
 	public function __construct()
 	{
@@ -15,231 +14,79 @@ class Auth extends CI_Controller
 	{
 		//verificar la session de usuario
 		$this->load->view('login');
-		$this->load->library('session');
-		if ($this->session->userdata()) {
-			$tip = (int)$this->session->userdata("usrtipo");
-			if ($tip == 1) {
-				redirect(base_url('Admin/Inicio'));
-			} else if ($tip == 2) {
-				redirect(base_url('Lab/Inicio'));
-			} else if ($tip == 4) {
-				redirect(base_url('Alumno/Inicio'));
-			}
-		}
 	}
 
-	public function login2()
+	public function Login()
 	{
-		$varablequevienedeJS = $_GET['c'];
-		// var_dump($varablequevienedeJS);
-		// $varablequevienedeJS = "6wxgIG0XN3XuADxgVLkZ5da3G2l2";
-		$jsonUrl = "https://applab-9034b.firebaseio.com/Usuarios/" . $varablequevienedeJS . ".json";
-
-		$result = json_decode(file_get_contents($jsonUrl));
-		$codtip = $result->rol;
-		$codlab = $result->codlab;		
-		$correo = $result->correo;
-		$nombre = $result->nombres;
-
-		$data  = array(
-			'usrcorreo' => $correo,
-			'usrnombre' => $nombre,
-			'usrtipo' => $codtip,
-			'login' => true,
-		);
-
-		//obtener el parametro de el mondre de laboratorio
-		if ($codtip == 'encargado') {
-			$_param = array(
-				'cod' => $codlab,
-				'edf' => 0,
-				'acr' => '',
-				'fil' => 0,
-				'col' => 0,
-				'lat' => 0,
-				'lon' => 0,
-				'nom' => '',
-				'est' => '',
-			);
-			$postData = '';
-			//Creamos arreglo nombre/valor separado por &
-			foreach ($_param as $k => $v) {
-				$postData .= $k . '=' . $v . '&';
-			}
-			rtrim($postData, '&');
-			//_________________________________________________________________
-			//_________________________________________________________________
-			//Recojo y arreglo los parametros
-			$url = URLWS . 'Laboratorio/Listado';
-			//_________________________________________________________________
-			//_________________________________________________________________
-			//creamos nuevo recurso cURL y su Conf (Esto mejor que ni se toque siempre va)
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_URL, $url);
-			curl_setopt($ch, CURLOPT_HEADER, false);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_USERAGENT, USERAGENTWS);
-			curl_setopt($ch, CURLOPT_COOKIE, COOKIECURL);
-			curl_setopt($ch, CURLOPT_AUTOREFERER, true);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-			curl_setopt($ch, CURLOPT_MAXREDIRS, 20);
-			curl_setopt($ch, CURLOPT_POST, count($_param));
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
-			//_________________________________________________________________
-			//Obtenemos el resultado    
-			$result = json_decode(curl_exec($ch));
-			//cerramos el Curl
-			curl_close($ch);
-			$objlab = $result->resp;
-			$labnombre = $objlab[0]->lab_nombre;
-			$data  = array(
-				'usrcorreo' => $correo,
-				'usrnombre' => $nombre,
-				'usrtipo' => $codtip,
-				'usrlab' => $codlab,
-				'usrlabnom' => $labnombre,
-				'login' => true,
-			);
-		}
-		$this->session->set_userdata($data);
-		// $user =  $this->session->userdata("usuario");
-		if ($codtip == "admin")
-			redirect(base_url('Admin/Inicio'));
-		else if ($codtip == "encargado")
-			redirect(base_url('Lab/Inicio'));
-		else
-			redirect(base_url('Alumno/Inicio'));
-	}
-
-	public function login()
-	{
-		$user = $this->input->post("txtUsr");
-		//_________________________________________________________________
-		//Recojo y arreglo los parametros
+		// $this->load->library('session');
+		// $agent = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)';
 		$_param = array(
-			'usr' => $user,
-			'cod' => 0,
-			'nom' => '',
-			'est' => '',
-			'con' => '',
-			'tip' => 0,
-			'fcod' => '',
+			'usr' => $this->input->post("txtUsr"),
+			'pass' => $this->input->post("txtPass"),
 		);
+		//__________________________________________________________________________________
 		$postData = '';
 		//Creamos arreglo nombre/valor separado por &
 		foreach ($_param as $k => $v) {
 			$postData .= $k . '=' . $v . '&';
 		}
 		rtrim($postData, '&');
-		//_________________________________________________________________
-		//_________________________________________________________________
-		//Recojo y arreglo los parametros
-		$url = URLWS . 'Usuario/Listado';
-		//_________________________________________________________________
-		//_________________________________________________________________
-		//creamos nuevo recurso cURL y su Conf (Esto mejor que ni se toque siempre va)
+
+		$url = URLWS . '/Fun/Login';
+		$ua = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.A.B.C Safari/525.13';
 		$ch = curl_init($url);
 		curl_setopt($ch, CURLOPT_URL, $url);
+
 		curl_setopt($ch, CURLOPT_HEADER, false);
+
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_USERAGENT, USERAGENTWS);
-		curl_setopt($ch, CURLOPT_COOKIE, COOKIECURL);
+		curl_setopt($ch, CURLOPT_USERAGENT, $ua);
+		curl_setopt($ch, CURLOPT_COOKIE, 'NID=67=pdjIQN5CUKVn0bRgAlqitBk7WHVivLsbLcr7QOWMn35Pq03N1WMy6kxYBPORtaQUPQrfMK4Yo0vVz8tH97ejX3q7P2lNuPjTOhwqaI2bXCgPGSDKkdFoiYIqXubR0cTJ48hIAaKQqiQi_lpoe6edhMglvOO9ynw; PREF=ID=52aa671013493765:U=0cfb5c96530d04e3:FF=0:LD=en:TM=1370266105:LM=1370341612:GM=1:S=Kcc6KUnZwWfy3cOl; OTZ=1800625_34_34__34_; S=talkgadget=38GaRzFbruDPtFjrghEtRw; SID=DQAAALoAAADHyIbtG3J_u2hwNi4N6UQWgXlwOAQL58VRB_0xQYbDiL2HA5zvefboor5YVmHc8Zt5lcA0LCd2Riv4WsW53ZbNCv8Qu_THhIvtRgdEZfgk26LrKmObye1wU62jESQoNdbapFAfEH_IGHSIA0ZKsZrHiWLGVpujKyUvHHGsZc_XZm4Z4tb2bbYWWYAv02mw2njnf4jiKP2QTxnlnKFK77UvWn4FFcahe-XTk8Jlqblu66AlkTGMZpU0BDlYMValdnU; HSID=A6VT_ZJ0ZSm8NTdFf; SSID=A9_PWUXbZLazoEskE; APISID=RSS_BK5QSEmzBxlS/ApSt2fMy1g36vrYvk; SAPISID=ZIMOP9lJ_E8SLdkL/A32W20hPpwgd5Kg1J');
+
 		curl_setopt($ch, CURLOPT_AUTOREFERER, true);
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 		curl_setopt($ch, CURLOPT_MAXREDIRS, 20);
-		curl_setopt($ch, CURLOPT_POST, count($_param));
+		curl_setopt($ch, CURLOPT_POST, count($postData));
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
-		//_________________________________________________________________
-		//Obtenemos el resultado    
-		$result = json_decode(curl_exec($ch));
-		//cerramos el Curl
+
+		$output = curl_exec($ch);
+		$val = json_decode($output);
+		$last = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
 		curl_close($ch);
-		$objusr = $result->resp;
-		$usrusuario = $objusr[0]->usr_usuario;
-		$usrnombre = $objusr[0]->usr_nombre;
-		$codtip = (int)$objusr[0]->usr_codtip;
-		$codlab = (int)$objusr[0]->usr_codlab;
-		// echo($result);
-		// var_dump($result->resp[0]->usr_codtip);
-		// var_dump((int)$objusr[0]->usr_codtip);
-		if ($codtip > 0) {
-			$login = true;
-		} else
-			$login = false;
+		// return array($result, $last);
+
+		//__________________________________________________________________________________
+
+		// if ($this->input->post("txtUsr") == 'admin@mail.com')
+		if ($output)
+			$valida = true;
+		else
+			$valida = false;
 
 		//si el nombre de usuario ingresado es correcto enviarlo al controlador Dashboard
-		if ($login == true) {
+		if ($valida == true) {
 			$data  = array(
-				'usrcorreo' => $usrusuario,
-				'usrnombre' => $usrnombre,
-				'usrtipo' => $codtip,
-				'login' => $login,
+				'usuario' => $_param['usr'],
+				'login' => $valida,
+				'nom' => $val[0]->{'usr_correo'},
+				// 'va' => $val['_log'],
+				// 'ou' => $output,
+				// 'pd' => $postData,
 			);
-
-			//obtener el parametro de el mondre de laboratorio
-			if ($codtip == 2) {
-				$_param = array(
-					'cod' => $codlab,
-					'edf' => 0,
-					'acr' => '',
-					'fil' => 0,
-					'col' => 0,
-					'lat' => 0,
-					'lon' => 0,
-					'nom' => '',
-					'est' => '',
-				);
-				$postData = '';
-				//Creamos arreglo nombre/valor separado por &
-				foreach ($_param as $k => $v) {
-					$postData .= $k . '=' . $v . '&';
-				}
-				rtrim($postData, '&');
-				//_________________________________________________________________
-				//_________________________________________________________________
-				//Recojo y arreglo los parametros
-				$url = URLWS . 'Laboratorio/Listado';
-				//_________________________________________________________________
-				//_________________________________________________________________
-				//creamos nuevo recurso cURL y su Conf (Esto mejor que ni se toque siempre va)
-				$ch = curl_init($url);
-				curl_setopt($ch, CURLOPT_URL, $url);
-				curl_setopt($ch, CURLOPT_HEADER, false);
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-				curl_setopt($ch, CURLOPT_USERAGENT, USERAGENTWS);
-				curl_setopt($ch, CURLOPT_COOKIE, COOKIECURL);
-				curl_setopt($ch, CURLOPT_AUTOREFERER, true);
-				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-				curl_setopt($ch, CURLOPT_MAXREDIRS, 20);
-				curl_setopt($ch, CURLOPT_POST, count($_param));
-				curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
-				//_________________________________________________________________
-				//Obtenemos el resultado    
-				$result = json_decode(curl_exec($ch));
-				//cerramos el Curl
-				curl_close($ch);
-				$objlab = $result->resp;
-				$labnombre = $objlab[0]->lab_nombre;
-				$data  = array(
-					'usrcorreo' => $usrusuario,
-					'usrnombre' => $usrnombre,
-					'usrtipo' => $codtip,
-					'usrlab' => $codlab,
-					'usrlabnom' => $labnombre,
-					'login' => $login,
-				);
-			}
-
+			// $this->session->set_userdata('logged_in', $session_data);	
 			$this->session->set_userdata($data);
-			// $user =  $this->session->userdata("usuario");
-			if ($codtip == 1)
-				redirect(base_url('Admin/Inicio'));
-			else if ($codtip == 2)
-				redirect(base_url('Lab/Inicio'));
-			else if ($codtip == 4)
-				redirect(base_url('Alumno/Inicio'));
+
+			// $usr =  $this->session->userdata("usuario");			
+			redirect(base_url('/Edificios'));
+			// if($usr=="estudiante")
+			// 	redirect(base_url() . "c_estudiante/Inicio");
+			// else if($usr=="admin")
+			// redirect(base_url() . "c_admin/Inicio");
+			// else if($usr=="lab")
+			// redirect(base_url() . "c_laboratorio/Inicio");
+
 		} else
-			redirect(base_url());
+			redirect(base_url('Login'));
 	}
 
 	public function logout()
@@ -247,6 +94,6 @@ class Auth extends CI_Controller
 		session_start();
 		session_unset();
 		session_destroy();
-		redirect(base_url('/Login'));
+		redirect(base_url('Login'));
 	}
 }
